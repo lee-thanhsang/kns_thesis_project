a249bf6173bd90aefb03cb52f69df5717682c0be
simonle-novobi <simon.le@novobi.com>
2023-04-26 18:48:33 +0700

    [ADD] Adjust sơme pattern

--- app/services/pattern_responce_sentence.py
+++ app/services/pattern_responce_sentence.py
@@ -1,76 +1,83 @@
 name = [
-    'Sự kiện bạn cần tìm là KEYWORD.',
-    'Sự kiện phù hợp cho bạn là KEYWORD.',
     'Hoạt động bạn cần tìm là KEYWORD.',
-    'Sự kiện thích hợp cho bạn là KEYWORD.'
+    'Hoạt động phù hợp cho bạn là KEYWORD.',
+    'Hoạt động thích hợp cho bạn là KEYWORD.',
+    'Hoạt động bạn đang quan tâm có tên là KEYWORD.',
+    'Thông tin hoạt động mà bạn muốn tìm kiếm là KEYWORD.'
 ]
 
 name_request = [
-    'Bạn có biết tên của hoạt động này đó là gì không á?',
-    'Bạn cho ad biết tên của hoạt động đó được không?',
-    'Bạn ơi, ad cần thêm thông tin về tên của hoạt động á, bạn có biết hoạt động tên gì không?',
-    'Bạn cho ad biết tên của hoạt động để có thể hỗ trợ chính xác ho bạn nha.'
+    'Bạn có biết tên của hoạt động này là gì không?',
+    'Bạn cho mình biết tên của hoạt động đó được không?',
+    'Bạn ơi, mình cần thêm thông tin về tên của hoạt động á, bạn có biết hoạt động tên gì không?',
+    'Bạn cho mình biết tên của hoạt động để có thể hỗ trợ chính xác hơn nha.',
+    'Bạn có thể cho mình biết tên chính xác của hoạt động này được không?'
 ]
 
 contact = [
-    'Để biết thêm thông tin của sự kiện, bạn liên hệ KEYWORD.',
-    'Mọi thắc mắc bạn liên hệ KEYWORD nha.',
-    'Thông tin liên hệ của hoạt động: KEYWORD.',
+    'Để biết thêm thông tin của hoạt động, bạn có thể liên hệ KEYWORD.',
+    'Mọi thắc mắc bạn liên hệ đến KEYWORD nha.',
+    'Thông tin liên hệ của hoạt động này là: KEYWORD.',
     'Bạn vui lòng liên hệ KEYWORD nếu có bất kì vấn đề nào nhé.'
 ]
 
 host = [
-    'Sự kiện này được tổ chức bởi KEYWORD.',
+    'Hoạt động này được tổ chức bởi KEYWORD.',
     'Hoạt động này được KEYWORD tổ chức.',
-    'Đây là hoạt động được tổ chức bởi KEYWORD.'
+    'Đây là hoạt động được tổ chức bởi KEYWORD.',
+    'KEYWORD sẽ là đơn vị tổ chức hoạt động này',
+    'Tên của đơn vị tổ chức hoạt động này là KEYWORD.'
 ]
 
 host_request = [
-    'Bạn cho mình biết ai là người tổ chức hoạt động này được không?',
-    'Bạn có biết đơn vị nào là BTC của hoạt động này không á?',
-    'Bạn có thể cho mình thêm thông tin của đơn vị nắm chính sự kiện này được không?'
+    'Bạn cho mình biết người hoặc đơn vị tổ chức hoạt động này được không?',
+    'Bạn cho mình xin thông tin về người hoặc đơn vị tổ chức hoạt động này nhé?',
+    'Bạn có biết đơn vị nào là BTC của hoạt động này không?',
+    'Bạn có thể cho mình thêm thông tin của đơn vị nắm chính hoạt động này được không?',
+    'Bạn cung cấp cho mình thêm thông tin về người hoặc đơn vị tổ chức hoạt động này nhé!'
 ]
 
 job_description = [
     'Một số thông tin của hoạt động này: KEYWORD',
-    'Về sự kiện này, đây là một số thông tin nhé: KEYWORD'
+    'Về hoạt động này, đây là một số thông tin nhé: KEYWORD'
 ]
 
 job_description_request = [
-    "Bạn có thể cho mình thông tin chi tiết hơn về công việc mà mình sẽ làm trong hoạt động này được không?",
-    "Bạn cung cấp cho mình thêm thông tin mô tả công việc của hoạt động này nhé?",
-    "Bạn có thể cho mình biết mô tả công việc của hoạt động này là gì không?",
-    "Hoạt động này có mô tả công việc là gì vậy bạn?",
+    'Bạn có thể cho mình thông tin chi tiết hơn về công việc mà mình sẽ làm trong hoạt động này được không?',
+    'Bạn cung cấp cho mình thêm thông tin mô tả công việc của hoạt động này nhé?',
+    'Bạn có thể cho mình biết mô tả công việc của hoạt động này là gì không?',
+    'Hoạt động này có mô tả công việc là gì vậy bạn?',
     'Bạn có thể cung cấp thông tin về công việc mà bạn sẽ thực hiện trong hoạt động được không?'
 ]
  
 number = [
     'Số người tham gia hoạt động này vào khoảng KEYWORD người.',
     'Hoạt động này cần KEYWORD người tham gia.',
-    'Sự kiện này thu hút khoảng KEYWORD người tham gia.'
+    'Hoạt động này có khoảng KEYWORD người tham gia.'
 ]
 
 register_way = [
-    'Cách thức đăng kí tham gia: KEYWORD',
-    'Để đăng kí tham gia sự kiện: KEYWORD'
+    'Cách thức đăng kí tham gia của hoạt động này là: KEYWORD',
+    'Để đăng kí tham gia hoạt động bạn có thể: KEYWORD',
+    'Bạn có thể: KEYWORD để có thể đăng kí tham gia hoạt động này nhé. '
 ]
 
 requirement = [
-    'Một số yêu cầu của hoạt động này: KEYWORD.',
-    'Bạn phải đảm bảo một sô điều sau khi đăng kí tham gia hoạt động này: KEYWORD.',
+    'Một số yêu cầu của hoạt động này mà bạn nên chú ý: KEYWORD.',
+    'Bạn phải đảm bảo một số điều kiện sau khi đăng kí tham gia hoạt động này: KEYWORD.',
     'Để tham gia hoạt động, bạn phải có đủ các yêu cầu sau: KEYWORD.'
 ]
 
 benefit_drl = [
-    'Khi tham gia hoạt động này, bạn có thể nhận được KEYWORD điểm rèn luyện.',
-    'Bạn có thể nhận được KEYWORD điểm rèn luyện khi tham gia sự kiện này nhé.',
-    'Quyền lợi khi tham gia hoạt động: KEYWORD điểm rèn luyện.'
+    'Khi tham gia hoạt động này, bạn sẽ nhận được KEYWORD điểm rèn luyện.',
+    'Bạn sẽ nhận được KEYWORD điểm rèn luyện khi tham gia hoạt động này nhé.',
+    'Quyền lợi khi bạn tham gia hoạt động này là: KEYWORD điểm rèn luyện.'
 ]
 
 benefit_ctxh = [
     'Khi tham gia hoạt động này, bạn có thể nhận được KEYWORD ngày ctxh.',
-    'Bạn có thể nhận được KEYWORD ngày ctxh khi tham gia sự kiện này nhé.',
-    'Quyền lợi khi tham gia hoạt động: KEYWORD ngày ctxh.'
+    'Bạn có thể nhận được KEYWORD ngày ctxh khi tham gia hoạt động này nhé.',
+    'Quyền lợi khi bạn tham gia hoạt động này là: KEYWORD ngày ctxh.'
 ] 
 
 benefit_others = [
@@ -79,62 +86,69 @@ benefit_others = [
 ]
 
 time_start = [
-    'Hoạt động này diễn ra từ KEYWORD.',
+    'Hoạt động này diễn ra lúc KEYWORD.',
     'Thời gian bắt đầu hoạt động là KEYWORD.',
-    'Thời gian diễn ra sự kiện là KEYWORD.',
-    'Sự kiện này bắt đầu vào KEYWORD.'
+    'Thời gian diễn ra hoạt động là KEYWORD.',
+    'Hoạt động này bắt đầu vào KEYWORD.',
+    'Thời gian hoạt động diễn ra là KEYWORD.',
 ]
 
 time_end = [
     'Hoạt động này diễn ra đến KEYWORD.',
-    'Thời gian kết thúc hoạt động là KEYWORD.',
-    'Thời gian sự kiện kết thúc là KEYWORD.',
-    'Sự kiện này kết thúc vào KEYWORD.'
+    'Thời gian kết thúc của hoạt động là KEYWORD.',
+    'Thời gian hoạt động này kết thúc là KEYWORD.',
+    'Hoạt động này kết thúc vào KEYWORD.'
 ]
  
 register_time_start = [
-    'Thời gian đăng kí sự kiện bắt đầu từ KEYWORD.',
-    'Từ KEYWORD, bạn có thể đăng kí hoạt động này.',
+    'Thời gian đăng kí tham gia hoạt động bắt đầu từ KEYWORD nhé.',
+    'Từ KEYWORD, bạn có thể đăng kí tham gia hoạt động này.',
     'Hoạt động sẽ mở đăng kí từ KEYWORD.',
-    'Sự kiện này có thể đăng kí từ KEYWORD.'
+    'Hoạt động này có thể đăng kí vào lúc KEYWORD.'
 ]
  
 register_time_end = [
-    'Thời gian đăng kí sự kiện kết thúc vào KEYWORD.',
+    'Thời gian đăng kí hoạt động kết thúc vào KEYWORD.',
     'Bạn có thể đăng kí hoạt động này đến KEYWORD.',
-    'Hoạt động sẽ mở đăng kí đến KEYWORD.',
-    'Sự kiện này kết thúc đăng kí lúc KEYWORD.'
+    'Hoạt động này mở đăng kí đến KEYWORD.',
+    'Hoạt động này kết thúc đăng kí lúc KEYWORD.'
 ]
 
 place = [
-    'Sự kiện này diễn ra ở KEYWORD.',
-    'Sự kiện này được tổ chức tại KEYWORD.',
-    'KEYWORD sẽ là nơi tổ chức sự kiện này nhé.',
-    'KEYWORD sẽ là nơi diễn ra hoạt động này nhé.',
-    'Địa điểm tổ chức sự kiện này là KEYWORD.',
-    'Sự kiện này được tổ chức tại KEYWORD.'
+    'Hoạt động này diễn ra ở KEYWORD.',
+    'Hoạt động này được tổ chức tại KEYWORD.',
+    'KEYWORD là nơi tổ chức hoạt động này nhé.',
+    'KEYWORD là nơi diễn ra hoạt động này nhé.',
+    'Địa điểm tổ chức hoạt động này là KEYWORD.',
+    'Hoạt động này được tổ chức tại KEYWORD nhé.'
 ]
 
 place_request = [
     'Bạn cho mình biết nơi diễn ra hoạt động được không?',
-    'Bạn có biết sự kiện này được diễn ra ở đâu không?',
-    'Bạn ơi, bạn cho ad biết hoạt động tổ chức ở đâu được không?',
-    'Bạn có thể cho mình biết địa điểm diễn ra hoạt động này không?'
+    'Bạn có biết hoạt động này được diễn ra ở đâu không?',
+    'Bạn ơi, bạn cho mình biết hoạt động này tổ chức ở đâu được không?',
+    'Bạn có thể cho mình biết địa điểm diễn ra hoạt động này không?',
+    'Mình muốn biết địa điểm của hoạt động này, bạn có thể giúp tôi được không?'
 ]
 
 greeting = [
     'Chào bạn, chúc bạn một ngày tốt lành. Mình có thể giúp gì cho bạn?',
-    'Chào bạn, mình có thể giúp gì cho bạn.'
+    'Chào bạn, mình hy vọng bạn đang có một ngày đầy năng lượng. Có điều gì mình có thể giúp bạn không?',
+    'Xin chào, mình rất vui được gặp bạn. Bạn có cần mình giúp gì không?',
+    'Xin chào, chúc bạn một ngày vui vẻ. Mình có thể giúp bạn gì không?'
 ]
 
 meaningless = [
-    """Xin lỗi, tôi chưa hiểu thông tin mà bạn muốn truyền tải.
-    Vui lòng thử lại với những thông tin rõ ràng hơn nhé."""
+    "Xin lỗi, tôi chưa hiểu thông tin mà bạn muốn truyền tải.\nVui lòng thử lại với những thông tin rõ ràng hơn nhé."
 ]
  
 complete = [
     'Cảm ơn bạn, chúc bạn có một ngày tốt lành.',
-    'Tạm biệt, chúc bạn có một ngày tốt lành.'
+    'Tạm biệt bạn, chúc bạn có một ngày tốt lành.',
+    'Cảm ơn bạn đã tương tác với mình. Chúc bạn có một ngày tốt lành!',
+    'Cảm ơn bạn đã cho mình cơ hội được hỗ trợ. Chúc bạn có một ngày tốt lành!',
+    'Cảm ơn bạn đã sử dụng mình? Hi vọng bạn sẽ có một trải nghiệm tốt!',
+    'Mình cảm ơn bạn đã ghé thăm. Chúc bạn có một ngày tuyệt vời!'
 ]
 
 time_request = [
